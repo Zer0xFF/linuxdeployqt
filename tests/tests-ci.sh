33780eaa4b3f5ecb077dd0c37ba8a0c4b94ee17d
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2019-10-21 14:39:45 +0100

    test ARM64 build

--- tests/tests-ci.sh
+++ tests/tests-ci.sh
@@ -2,12 +2,14 @@
 
 set -x
 
-source /opt/qt*/bin/qt*-env.sh
-/opt/qt*/bin/qmake CONFIG+=release CONFIG+=force_debug_info linuxdeployqt.pro
-# make -j$(nproc) # Not doing here but below with "pvs-tool trace"
 ARCH=`dpkg --print-architecture`
 if [ "$ARCH" == "amd64" ]
 then
+
+source /opt/qt*/bin/qt*-env.sh
+/opt/qt*/bin/qmake CONFIG+=release CONFIG+=force_debug_info linuxdeployqt.pro
+# make -j$(nproc) # Not doing here but below with "pvs-tool trace"
+
 # Test
 wget -q -O - http://files.viva64.com/etc/pubkey.txt | sudo apt-key add -
 sudo wget -O /etc/apt/sources.list.d/viva64.list http://files.viva64.com/etc/viva64.list
@@ -18,6 +20,12 @@ pvs-studio-analyzer trace -- make -j$(nproc)
 pvs-studio-analyzer analyze -e /opt -e /usr -o pvs-studio.log -j $(nproc) -l ./licence.lic
 plog-converter -a GA:1,2 -t tasklist -o pvs-studio-report.txt pvs-studio.log
 rm ./licence.lic
+
+else
+
+qmake CONFIG+=release CONFIG+=force_debug_info linuxdeployqt.pro
+which qmake
+
 fi
 
 # exit on failure
