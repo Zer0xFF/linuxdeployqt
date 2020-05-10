76032be74678ea9cebb65334e31c3a8823510d3b
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2020-05-10 04:40:04 +0100

    fix copy

--- tests/tests-environment.sh
+++ tests/tests-environment.sh
@@ -10,33 +10,28 @@ make -j2
 sudo make install
 
 cd -
+cd /tmp/
 
 ARCH=`dpkg --print-architecture`
 if [ "$ARCH" == "amd64" ]
 then
 	sudo add-apt-repository --yes ppa:beineri/opt-qt58-trusty
 	sudo apt-get update -qq
-
-	cd /tmp/
 	wget -c "https://github.com/probonopd/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
-	chmod +x appimagetool*AppImage
-	./appimagetool*AppImage --appimage-extract
-	sudo cp squashfs-root/usr/bin/* /usr/local/bin
-	cd -
 
 	sudo apt-get -y install qt58base qt58declarative qt58webengine binutils xpra
 
 else
 	sudo apt-get update -qq
 	sudo apt install -y zlib1g zlib1g-dev
-	cd /tmp/
 	wget -c "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-aarch64.AppImage"
-	chmod +x appimagetool*AppImage
-	./appimagetool*AppImage --appimage-extract
-	sudo cp squashfs-root/usr/bin/* /usr/local/bin/
-	sudo cp -r squashfs-root/usr/lib/appimagekit /usr/local/lib/
-	sudo chmod +rx /usr/local/lib/appimagekit
-	cd -
 
 	sudo apt install -y --no-install-recommends cmake gcc-5 g++-5 qt5-qmake pkg-config fuse file qtbase5-dev desktop-file-utils xpra
 fi
+
+chmod +x appimagetool*AppImage
+./appimagetool*AppImage --appimage-extract
+sudo cp squashfs-root/usr/bin/* /usr/local/bin/
+sudo cp -r squashfs-root/usr/lib/appimagekit /usr/local/lib/
+sudo chmod +rx /usr/local/lib/appimagekit
+cd -
